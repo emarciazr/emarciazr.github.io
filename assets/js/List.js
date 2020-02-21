74e8e45d97a82e075b1711646ecd4de367307e8e
Eva Marcia Zavala Rodríguez <emarciazr@kamikaze.mx>
2020-02-21 02:47:55 -0800

    Add files via upload

--- assets/js/List.js
+++ assets/js/List.js
@@ -3,63 +3,73 @@ var myArray = [
         "Tipo": "Consulting Service",
         "Nombre": "Software Reverse Engineering",
         "Referencia": "detail.html?id=0",
-        "Imagen":"images/p01.jpg",
+        "Imagen":"images/p00.jpg",
         "Resumen": "Analysis with OllyDbg to a customer developed aplicattion with source code missing"   
     },
     {
-        "Tipo": "Sofware Development 01",
-        "Nombre": "QRCode DeviceID Label Printing",
+        "Tipo": "Sofware Upgrade",
+        "Nombre": "SATO labels on automated weighing system",
         "Referencia": "detail.html?id=1",
-        "Imagen":"images/pic02.jpg",
-        "Resumen": "Device ID Generator and QRCode Label printing"
+        "Imagen":"images/p01.jpg",
+        "Resumen": "Software upgrade on Avery Weightronix Devices",
     },
     {
-        "Tipo": "Sofware Development 02",
-        "Nombre": "QRCode DeviceID Label Printing",
-        "Referencia": "detail.html?id=1",
-        "Imagen":"images/pic02.jpg",
-        "Resumen": "Device ID Generator and QRCode Label printing"
+        "Tipo": "Software Development",
+        "Nombre": "QRCode Device ID Label Printing",
+        "Referencia": "detail.html?id=2",
+        "Imagen":"images/p02.jpg",
+        "Resumen": "Device ID Generator and QRCode Label printing",
     },
     {
-        "Tipo": "Sofware Development 03",
-        "Nombre": "QRCode DeviceID Label Printing",
-        "Referencia": "detail.html?id=1",
-        "Imagen":"images/pic02.jpg",
-        "Resumen": "Device ID Generator and QRCode Label printing"
-    }]
+        "Tipo": "Software Development",
+        "Nombre": "Control de Bombas Punta Faro",
+        "Referencia": "detail.html?id=3",
+        "Imagen":"images/p03.jpg",
+        "Resumen": "Real Time Monitoring automated sea water pump station",
+    },
+    {
+        "Tipo": "Software Development",
+        "Nombre": "RPM Navico Shopfloor Control",
+        "Referencia": "detail.html?id=4",
+        "Imagen":"images/p04.jpg",
+        "Resumen": "Real time Monitoring produccion process, Packing, QA, Reports"
+    },
+    {
+        "Tipo": "Software Development",
+        "Nombre": "QDL Order Receipt",
+        "Referencia": "detail.html?id=5",
+        "Imagen":"images/p05.jpg" ,
+        "Resumen": "Software for handheld terminal MC3190, scan an store data information from orders received"
+
+    }
+
+    ]
 
 var itm0 = Item(0)
 var itm1 = Item(1)
 var itm2 = Item(2)
 var itm3 = Item(3)
-var foo = AllItems()
+var itm4 = Item(4)
+var itm5 = Item(5)
+// var itm6 = Item(6)
+// var itm7 = Item(7)
+// var itm8 = Item(8)
+// var itm9 = Item(9)
+// var itm9 = Item(10)
+
 
 document.getElementById("itm0").innerHTML = itm0;
 document.getElementById("itm1").innerHTML = itm1;
 document.getElementById("itm2").innerHTML = itm2;
 document.getElementById("itm3").innerHTML = itm3;
- document.getElementById("foo").innerHTML = foo;
- function AllItems()
- {
-    var txt='<section class="posts">';
-    for (var i = 0 ; i<myArray.length ; i++)
-    {
-        txt = txt +  
-        '<article>' +
-        '<header>' +
-            '<span class="date">'+myArray[i].Tipo+'</span>'+
-            '<h2><a href="'+myArray[i].Referencia+'">'+myArray[i].Nombre+'</a></h2>'+
-        '</header>'+
-        '<a href="'+myArray[i].Referencia+'" class="image fit"><img src="'+myArray[i].Imagen+'" alt="" /></a>'+
-        '<p>'+myArray[i].Resumen+'.</p>'+
-        '<ul class="actions special">'+
-            '<li><a href="'+myArray[i].Referencia+'" class="button">View More</a></li>'+
-        '</ul>'+
-        '</article>';
-    }
-    txt = txt +'</section>'
-    return txt;
-}
+document.getElementById("itm4").innerHTML = itm4;
+document.getElementById("itm5").innerHTML = itm5;
+// document.getElementById("itm6").innerHTML = itm6;
+// document.getElementById("itm7").innerHTML = itm7;
+// document.getElementById("itm8").innerHTML = itm8;
+// document.getElementById("itm9").innerHTML = itm9;
+// document.getElementById("itm10").innerHTML = itm10;
+ 
 function Item(i)
 {
     var txt="";
--- otro.js
+++ otro.js
@@ -1,41 +1,87 @@
 var myArray = [
     {
         "Tipo": "Software Upgrade",
-        "Nombre": "BCS & PBCBA Software Reverse Engineering",
+        "Nombre": "BCS and PCBA Software ReverseEngineering",
         "Referencia": "detail.html?id=0",
-        "Imagen":"images/p01.jpg",
-        "Resumen": "Analysis with OllyDbg to a customer developed aplicattion with source code missing"  ,
+        "Imagen":"<img src= \"images/p00.jpg\" alt=\"\" />",
+        "Resumen": "Analysis with OllyDbg to a customer developed aplicattion with source code missing",
         "Tools":"OllyDBG & ASM",
-        "Cliente": "Navico",
+        "Cliente": "Navico, Ensenada",
         "Fecha":"January 2020",
-        "Descripcion":"In the last two years Navico migrate all DB Servers from SQL Server 2015 to SQL Server 2019.<br>"+
-                     "The min were obligated to mantain one Server with and old database, because they have two old applications, with connections to this old database. <br>"+
-                     "I analyse the exe files with Ollydbg to add a new db connection string and re-assemble the exe file. "
+        "Roles":"Analyst, Developer, Tester, Deployment",
+        "Descripcion":"In the last two years, Navico had migrated all their DB servers from SQL Server 2015 to 2019.<br>"+
+        "The main requirement was to keep one server with its old DBs structures to keep their old labelling applications running.<br>"+ 
+        "My job was to disassemble and analyze current executable files by using Ollydbg and add  new DB connection strings, then reassemble and deploy the modified files<br>"
     },
     {
-        "Tipo": "Sofware Development",
+        "Tipo": "Sofware Upgrade",
+        "Nombre": "SATO labels on automated weighing System",
+        "Referencia": "detail.html?id=2",
+        "Imagen":"<video src=\"images/v01.mp4\" autoplay muted loop></video>",
+        "Resumen": "Software upgrade on Avery Weightronix Devices",
+        "Tools":" Ztools AWT Basic, SATO E Pro  ",
+        "Cliente": "Grupo Bafar, Chihuahua",
+        "Fecha":"September, 2019",
+        "Roles":" Developer, Tester, Deployment",
+        "Descripcion":"Weighing software migration from E1310 Avery Weightronics(AWT) to ZM510 AWT.<br>"+
+        "Upgrade label templates to include new TIF regulations and SATO Automated Printers<br>"+
+        "Also create different label sizes with QRCode web page link to automated registration<br>"
+    },
+    {
+        "Tipo": "Software Development",
         "Nombre": "QRCode DeviceID Label Printing",
         "Referencia": "detail.html?id=1",
-        "Imagen":"images/pic02.jpg",
+        "Imagen":"<img src= \"images/p02.jpg\" alt=\"\" />",
         "Resumen": "Device ID Generator and QRCode Label printing",
-        "Tools":"OllyDBG & ASM",
-        "Cliente": "Navico",
-        "Fecha":"January 2020",
-        "Descripcion":"kkk<br> jjjjjjjj <br>ggggg"
+        "Tools":"Visual studio 2013 VB .NET, SQL SERVER",
+        "Cliente": "Navico, Ensenada",
+        "Fecha":"July 2019",
+        "Roles":"Analyst, Developer, Tester, Deployment",
+        "Descripcion":"The most of Navico devices have an on powering method to register device with a unique 12 characters Device ID.<br>"+
+        "Low Cost models, don't have this automatic method. This software generate the Device ID encoding part number, serial and family<br>"+
+        "Also create different label sizes with QRCode web page link to automated registration<br>"
     },
     {
-        "Tipo": "Sofware Development 02",
-        "Nombre": "QRCode DeviceID Label Printing",
-        "Referencia": "detail.html?id=1",
-        "Imagen":"images/pic02.jpg",
-        "Resumen": "Device ID Generator and QRCode Label printing"
+        "Tipo": "Software Development",
+        "Nombre": "Control de Bombas Punta Faro",
+        "Referencia": "detail.html?id=2",
+        "Imagen":"<a href = \"http://fcm.ens.uabc.mx/~bombas/\" target=\"_blank\"> <img src= \"images/p03.jpg\" alt=\"\" /></a>+",
+        "Resumen": "Real Time Monitoring automated sea water pump station",
+        "Tools": "Visual studio 2008 VB .NET, HTML, PHP, Rockwell Studio, Advanced HMI, MySQL",
+        "Cliente": "UABC, Ensenada",
+        "Fecha":"April 2015",
+        "Roles":"Desktop & Web Developer, Tester, Deployment",
+        "Descripcion":"Real Time Monitoring sea water pump station, automated by Allen Bradley 1769-L33ER PLC.<br>"+
+        "The pump station send water to three sea water tanks. The water is used for aquaculture research.<br>"+
+        "My Job was to develop software to load and config operation parameter, receive control and data signals from PLC to store on MySQL DB and"+
+        "<a href = \"http://fcm.ens.uabc.mx/~bombas/\" target=\"_blank\"> web page</a> developed with PHP to Show system current state."
     },
     {
-        "Tipo": "Sofware Development 03",
-        "Nombre": "QRCode DeviceID Label Printing",
-        "Referencia": "detail.html?id=1",
-        "Imagen":"images/pic02.jpg",
-        "Resumen": "Device ID Generator and QRCode Label printing"
+        "Tipo": "Software Development",
+        "Nombre": "RPM Navico Shopfloor Control",
+        "Referencia": "detail.html?id=3",
+        "Imagen":"<img src= \"images/p04.jpg\" alt=\"\" /></a>+",
+        "Resumen": "Real time Monitoring produccion process, Packing, QA, Reports",
+        "Tools": "Visual studio 2012 C#,VB .NET, HTML, ASP, MySQL, SQL Server",
+        "Cliente": "Navico, Ensenada",
+        "Fecha":"February 2015 - September 2016",
+        "Roles":"Project Manager, Analyst, Desktop Developer, Tester, Deployment",
+        "Descripcion":"Designed and developed a custom MRP System that among other things automates the "+
+        "processing of production data that is used for their metrics to better plan their production."+
+        "Included modules were: production shift management, production planning, production "+
+        "results, product catalogs and reports"
+    },
+    {
+        "Tipo": "Software Development",
+        "Nombre": "QDL Order Receipt",
+        "Referencia": "detail.html?id=5",
+        "Imagen":"images/p05.jpg" ,
+        "Resumen": "Software for handheld terminal MC3190, scan an store data information from orders received",
+        "Tools": "Visual studio 2013,VB .NET, SQL Server CE, Windows CE, windows 6.0",
+        "Cliente": "Navico, Ensenada",
+        "Fecha":"May 2014",
+        "Roles":"Analyst, hand held Developer, Tester, Deployment",
+        "Descripcion":""
     }]
 
 const urlParams = new URLSearchParams(window.location.search.substring(1));
@@ -43,10 +89,12 @@ var  idProject = urlParams.get("id");
 
 
 document.getElementById("Tipo").innerHTML = myArray[idProject].Tipo
-document.getElementById("Nombre").innerHTML = myArray[idProject].Nombre;
+document.getElementById("Proyecto").innerHTML = myArray[idProject].Nombre;
+document.getElementById("Titulo").innerHTML = myArray[idProject].Nombre;
 document.getElementById("Cliente").innerHTML = myArray[idProject].Cliente;
 document.getElementById("Descripcion").innerHTML = myArray[idProject].Descripcion;
 document.getElementById("Fecha").innerHTML = myArray[idProject].Fecha;
 document.getElementById("Tools").innerHTML = myArray[idProject].Tools;
-document.getElementById("Imagen").innerHTML = '<img src= "'+myArray[idProject].Imagen+'" alt="" />';
+document.getElementById("Roles").innerHTML = myArray[idProject].Roles;
+document.getElementById("Imagen").innerHTML = myArray[idProject].Imagen;
 
